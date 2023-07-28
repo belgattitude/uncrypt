47f2a4e6816c29c42cc2f296355119ea16b40ca2
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-07-28 14:38:16 +0200

    feat: build with tsup

---
+++ packages/simple-aes/tsup.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => {
+  return {
+    entry: ['src/index.ts'],
+    splitting: true,
+    clean: true,
+    dts: true,
+    format: ['esm', 'cjs'],
+    outExtension({ format }) {
+      return {
+        js: `.${format === 'cjs' ? 'cjs' : 'mjs'}`,
+      };
+    },
+    platform: 'node',
+    target: ["node16"],
+    tsconfig: './tsconfig.build.json',
+    sourcemap: !options.watch,
+    minify: !options.watch,
+  };
+});
